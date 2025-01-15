86cbce73e86bdd298d8fdf03f8c52d7aa2afb575
System Administrator <root@Jesse.local>
2025-01-15 13:04:08 -0700

    sudo-3d

---
+++ node_modules/sudo-3d/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  server: {
+    open: true,
+  },
+});
